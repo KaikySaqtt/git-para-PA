c2ccbaa7db30a1cbfb8a4575f69f0a1dadfdf749
Kaiky Saquettte Pereira <138088042+KaikySaqtt@users.noreply.github.com>
2023-11-27 10:30:28 -0300

    Add files via upload

---
+++ Conta letras/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Conta_letras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase, vLetra, espa;
+            int i, ContA=0;
+
+            Console.Clear();
+            Console.WriteLine("Me diga a frase: ");
+            frase = Console.ReadLine();
+            for (i = 0; i < frase.Length; i++) 
+            {
+                vLetra = frase.Substring(i, 1);
+                switch (vLetra.ToLower())
+                {
+                    case "a":
+                    case "ã":
+                    case "á":
+                        {
+                            ContA++;
+                            break;
+                        }
+                    default:
+                        break;
+                }
+                
+            }
+            Console.WriteLine($"A frase que foi digitaa tem {frase.} caracteres.");
+            Console.WriteLine($"E eu consegui encontra um total de {ContA} vogais 'a' ");
+
+        }
+    }
+}
